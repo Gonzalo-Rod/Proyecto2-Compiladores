06da567f30ba010738ec96a23358de5c6a91c497
Gonzalo-Rod <85299734+Gonzalo-Rod@users.noreply.github.com>
2024-07-09 13:00:44 -0500

    FCallStm imp_typechecker.hh

--- imp_typechecker.hh
+++ imp_typechecker.hh
@@ -47,6 +47,7 @@ public:
   void visit(FunDec*);
   void visit(StatementList*);
   void visit(AssignStatement*);
+  void visit(FCallstm*);
   void visit(PrintStatement*);
   void visit(IfStatement*);
   void visit(WhileStatement*);
